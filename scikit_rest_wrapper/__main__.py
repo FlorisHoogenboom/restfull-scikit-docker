c01ac4ed9e4dc48924cb1b270b5f298258c9475a
Floris Hoogenboom <floris@digitaldreamworks.nl>
2018-04-05 16:58:41 +0200

    Added files needed for debugging.

---
+++ scikit_rest_wrapper/__main__.py
@@ -0,0 +1,10 @@
+from scikit_rest_wrapper.bootstrap import App
+from scikit_rest_wrapper import config
+
+if __name__ == "__main__":
+    # Change directory specification for development
+    config.MODULES_DIR = '../modules'
+    config.OBJECTS_DIR = '../objects'
+
+    # Start the app
+    App().get_app().run(host='127.0.0.1', threaded=True, port=3000)
